ffc76ab2252b0ed5be434dd11bca05580c0c9731
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-02-28 12:35:15 -0600

    Initial Commit

---
+++ tableViewMiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewMiniApp
+//
+//  Created by Jonah Whitney on 2/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
